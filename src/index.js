e40fa45e54e60ab0bc90fb0e3bcbdae20f6e4eb4
Nikolai Nanev <niki4etooo@gmail.com>
2021-12-01 10:00:18 +0200

    feat: Main Menu, Settings Menu and Back navigation in the FlipFlop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import FlipFlop from './FlipFlop';
+import Settings from './Settings';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/FlipFlop" element={<FlipFlop />} />
+			<Route path="/Settings" element={<Settings />} />
+		</Routes>
+	</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
